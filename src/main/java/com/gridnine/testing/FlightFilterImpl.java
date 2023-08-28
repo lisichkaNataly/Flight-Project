8c5b9d30fc0351cabac089d220afb24b495aa3d0
natalamoiseeva <natalyatf@yandex.ru>
2023-08-28 10:31:08 +0500

    add method showFlight from FlightFilterImpl

--- src/main/java/com/gridnine/testing/FlightFilterImpl.java
+++ src/main/java/com/gridnine/testing/FlightFilterImpl.java
@@ -1,8 +1,20 @@
 package com.gridnine.testing;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class for filtering the list of flights
+ */
 public class FlightFilterImpl implements FlightFilter{
+
+    private final List<Segment> segs = new ArrayList<>();
+    private final LocalDateTime currentDateTime = LocalDateTime.now();
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private final List <Flight> tempListFlights = new ArrayList<>();
 
     @Override
     public void allFlightSegments(List<Flight> flights) {
@@ -22,5 +34,10 @@ public class FlightFilterImpl implements FlightFilter{
     @Override
     public List<Flight> theTotalTimeSpentOnTheGroundExceedsTwoHours(List<Flight> flights) {
         return null;
+    }
+
+    private void showFlight (Flight flight, LocalDateTime dep, LocalDateTime arr){
+        System.out.println("departure: " + dateTimeFormatter.format(dep)+ "\n"
+                + "arrival: " + dateTimeFormatter.format(arr) + "\n");
     }
 }
